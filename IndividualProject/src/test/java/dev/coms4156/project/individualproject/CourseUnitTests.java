fc17cb40d0016187d9dc396e93219dcef3ae0049
Marian Abuhazi <marian.abuhazi@gmail.com>
2024-09-08 23:47:29 -0400

    Full coverage units tests for Course.

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -2,7 +2,7 @@ package dev.coms4156.project.individualproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,8 +14,8 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration
 public class CourseUnitTests {
 
-  @BeforeAll
-  public static void setupCourseForTesting() {
+  @BeforeEach
+  public void setupCourseForTesting() {
     testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
   }
 
@@ -25,7 +25,76 @@ public class CourseUnitTests {
     assertEquals(expectedResult, testCourse.toString());
   }
 
+  @Test
+  public void enrollStudentSuceedsTest() {
+    boolean expectedResult = true;
+    int expectedEnrolledStudentCount = 1;
+    assertEquals(expectedResult, testCourse.enrollStudent());
+    assertEquals(expectedEnrolledStudentCount, testCourse.getEnrolledStudentCount());
+  }
+
+  @Test
+  public void enrollStudentFailsTest() {
+    boolean expectedResult = false;
+    testCourse.setEnrolledStudentCount(250);
+    assertEquals(expectedResult, testCourse.enrollStudent());
+  }
+
+  @Test
+  public void dropStudentSuceedsTest() {
+      boolean expectedResult = true;
+      int expectedEnrolledStudentCount = 0;
+      testCourse.setEnrolledStudentCount(1);
+      assertEquals(expectedResult, testCourse.dropStudent());
+      assertEquals(expectedEnrolledStudentCount, testCourse.getEnrolledStudentCount());
+  }
+
+  @Test
+  public void dropStudentFailsTest() {
+      boolean expectedResult = false;
+      testCourse.setEnrolledStudentCount(0);
+      assertEquals(expectedResult, testCourse.dropStudent());
+  }
+
+  @Test
+  public void getCourseLocationTest() {
+      String expectedResult = "417 IAB";
+      assertEquals(expectedResult, testCourse.getCourseLocation());
+  }
+
+  @Test
+  public void getInstructorNameTest() {
+      String expectedResult = "Griffin Newbold";
+      assertEquals(expectedResult, testCourse.getInstructorName());
+  }
+
+  @Test
+  public void getCourseTimeSlotTest() {
+      String expectedResult = "11:40-12:55";
+      assertEquals(expectedResult, testCourse.getCourseTimeSlot());
+  }
+
+  @Test
+  public void setInstructor() {
+      String expectedResult = "Marian Abuhazi";
+      testCourse.setInstructor("Marian Abuhazi");
+      assertEquals(expectedResult, testCourse.getInstructorName());
+  }
+
+  @Test
+  public void setLocation() {
+      String expectedResult = "Mudd 833";
+      testCourse.setLocation("Mudd 833");
+      assertEquals(expectedResult, testCourse.getCourseLocation());
+  }
+
+  @Test
+  public void setTime() {
+      String expectedResult = "10:10-11:25";
+      testCourse.setTime("10:10-11:25");
+      assertEquals(expectedResult, testCourse.getCourseTimeSlot());
+  }
+
   /** The test course instance used for testing. */
   public static Course testCourse;
 }
-
